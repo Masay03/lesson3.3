b3b33c3319ef4256b67430f87df50be939975420
Masay03 <maks.bz15@mail.ru>
2024-05-08 20:01:39 +0700

    add all cycle code

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+#Импортируем pygame
 import pygame
+#Импортируем рандом
 import random
 
+#Инициализируем pygame
 pygame.init()
 
 #Создаем окно и его параметры
@@ -14,7 +17,7 @@ icon = pygame.image.load("img/1674667984_grizly-club-p-klipart-tir-2.jpg")
 pygame.display.set_icon(icon)
 
 #Создаем целевой объект
-target_img = pygame.image.load("img/target.png")
+target_img = pygame.image.load("img/target.com.png")
 target_wiath = 80
 target_heigh = 80
 
@@ -22,10 +25,32 @@ target_heigh = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_wiath)
 target_y = random.randint(0, SCREEN_HEIGHT - target_heigh)
 
+#Цвет целевого объекта
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
+#Запускаем игру
 runing = True
 while runing:
-    pass
+    screen.fill(color)#Заливка экрана цветом
+    screen.blit(target_img, (target_x, target_y))#Рисуем целевой объект
+    pygame.display.update()#Обновляем экран
 
+    #Обработка событий
+    for event in pygame.event.get():
+        # Если нажали на крестик
+        if event.type == pygame.QUIT:
+            runing = False
+            # Если нажали на мышь
+        if event.type == pygame.MOUSEBUTTONDOWN:#Если нажали мышь
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            # Если мышь попал в целевой объект
+            if target_x < mouse_x < target_x + target_wiath and target_y < mouse_y < target_y + target_heigh:
+                target_x = random.randint(0, SCREEN_WIDTH - target_wiath)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_heigh)
+                color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+    #Обновляем экран
+    pygame.display.update()
+
+#Закрываем игру
 pygame.quit()
